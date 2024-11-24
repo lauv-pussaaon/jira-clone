2c4888abb967baabe8f357fe2442c008be51ebaa
Lauv <pussaaon@gmail.com>
2024-11-24 12:27:31 +0700

    sign-in and sign-up pages

--- src/app/(auth)/sign-up/page.tsx
+++ src/app/(auth)/sign-up/page.tsx
@@ -1,5 +1,9 @@
+"use client";
+
+import { SignUpCard } from "@/features/auth/components/sign-up-card";
+
 const SignInPage = () => {
-  return <div>SignUp</div>;
+  return <SignUpCard />;
 };
 
 export default SignInPage;
--- src/features/auth/components/sign-up-card.tsx
+++ src/features/auth/components/sign-up-card.tsx
@@ -1,31 +1,47 @@
 import { FcGoogle } from "react-icons/fc";
 import { FaGithub } from "react-icons/fa";
+import Link from "next/link";
 
 import { Button } from "@/components/ui/button";
 import {
 	Card,
 	CardContent,
+	CardDescription,
 	CardHeader,
 	CardTitle
 } from "@/components/ui/card";
 import { DottedSeparator } from "@/components/ui/dotted-separator";
 import { Input } from "@/components/ui/input";
 
-
-
-export const SignInCard = () => {	
+export const SignUpCard = () => {	
 	return (
 		<Card className="w-full h-full md:w-[487px] border-none shadow-none">
 			<CardHeader className="flex items-center justify-center text-center p-7">
 				<CardTitle className="text-2xl">
-					Welcome back!
+					Sign Up
 				</CardTitle>
+				<CardDescription>
+					By signing up, you agree to our 
+					<Link href="/terms">Terms of Service</Link>
+					{" and "}
+					<Link href="/privacy">
+						<span className="text-blue-700">Privacy Policy</span>
+					</Link>
+				</CardDescription>
 			</CardHeader>
 			<div className="px-7">
 				<DottedSeparator />
 			</div>
 			<CardContent className="p-7">
 				<form className="space-y-4">
+					<Input
+						required
+						type="text"
+						value={""}
+						onChange={() => {}}
+						placeholder="Enter your name"
+						disabled={false}
+					/>
 					<Input
 						required
 						type="email"
